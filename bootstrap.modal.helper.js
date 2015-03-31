cd76cb4439772165723a9d9ae585161884c2f70c
deflopian <deflopian@gmail.com>
2015-03-31 13:37:12 +0300

    update http to ssh

---
+++ bootstrap.modal.helper.js
@@ -0,0 +1,8 @@
+﻿//(function ($) {
+//    var showEvt = "show.bs.modal",
+//        hideEvt = "hide.bs.modal",
+//        $document = jQuery(document),
+//        $layout = jQuery(".b-layout"),
+//        Modal = $.fn.modal.Constructor;
+//    jQuery(document).on()
+//})(jQuery)
